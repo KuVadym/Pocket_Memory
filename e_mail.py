a4b2dbfc4889e88d724036562b42b8c43c4fbfc4
Vadym Kuzyk <kuzik94vadim@gmail.com>
2023-05-03 20:57:22 +0300

    working email sender

--- e_mail.py
+++ e_mail.py
@@ -3,17 +3,19 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from decouple import config
 from core.config import settings
+from sendgrid.helpers.mail import (From, To, PlainTextContent, HtmlContent)
 
 
 def send_email(to_email, subject, body):
     message = Mail(
-        from_email='your_email@example.com',
-        to_emails=to_email,
+        from_email=From('kuzik94vadim@gmail.com'),
+        to_emails=To(to_email),
         subject=subject,
-        html_content=body)
+        plain_text_content=PlainTextContent(body))
     try:
+        mail_json = message.get()
         sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
-        response = sg.send(message)
+        response = sg.send(mail_json)
         print(response.status_code)
         print(response.body)
         print(response.headers)
